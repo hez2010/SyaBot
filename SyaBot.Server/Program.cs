8fbaa4ccda964b5f88fe82865d0a4fb64c6037a7
Steven He <hez2010@outlook.com>
2021-04-08 00:47:18 +0800

    feat: record MethodInfo

--- SyaBot.Server/Program.cs
+++ SyaBot.Server/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -11,8 +12,9 @@ namespace SyaBot.Server
 {
     class UdpContext<T> where T : RequestModel
     {
-        public UdpContext(IPEndPoint? remote, T data)
+        public UdpContext(MethodInfo method, IPEndPoint? remote, T data)
         {
+            Method = method;
             RemoteEndPoint = remote;
             Data = data;
         }
@@ -20,21 +22,23 @@ namespace SyaBot.Server
         public IPEndPoint? RemoteEndPoint { get; }
 
         public T Data { get; }
+
+        public MethodInfo Method { get; }
     }
 
     class Program
     {
         static readonly UdpClient client = new(40080, AddressFamily.InterNetwork);
-        static readonly ConcurrentDictionary<string, (Type ModelType, Func<object, Task> Handler)> handlers = new();
+        static readonly ConcurrentDictionary<string, (Type ModelType, Func<object, Task> Handler, MethodInfo Method)> handlers = new();
 
-        static object? CreateUdpContext(Type dataType, IPEndPoint? remote, object data)
+        static object? CreateUdpContext(Type dataType, MethodInfo method, IPEndPoint? remote, object data)
         {
-            return Activator.CreateInstance(typeof(UdpContext<>).MakeGenericType(dataType), remote, data);
+            return Activator.CreateInstance(typeof(UdpContext<>).MakeGenericType(dataType), method, remote, data);
         }
 
         static void AddHandler<T>(Func<UdpContext<T>, Task> handler) where T : RequestModel
         {
-            handlers[typeof(T).ToString()] = (typeof(T), d => handler((UdpContext<T>)d));
+            handlers[typeof(T).ToString()] = (typeof(T), d => handler((UdpContext<T>)d), handler.Method);
         }
 
         static async Task Main(string[] args)
@@ -51,14 +55,14 @@ namespace SyaBot.Server
                 Console.WriteLine($"Request id: {request.Id}, type: {request.Type}");
 
                 if (!handlers.ContainsKey(request.Type)) continue;
-                var (type, handler) = handlers[request.Type];
+                var (type, handler, method) = handlers[request.Type];
 
                 var data = JsonSerializer.Deserialize(result.Buffer, type);
                 if (data is null) continue;
 
-                Console.WriteLine($"Map handler: {handler}");
+                Console.WriteLine($"Map handler: {method}");
 
-                var context = CreateUdpContext(type, result.RemoteEndPoint, data);
+                var context = CreateUdpContext(type, method, result.RemoteEndPoint, data);
                 if (context is null) continue;
 
                 await handler(context);
